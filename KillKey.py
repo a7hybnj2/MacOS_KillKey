53b2a78805c65a7632ba803c1eaa114e9637812d
ayhybnj2 <36831279+a7hybnj2@users.noreply.github.com>
2020-01-02 23:52:10 -0500

    cleaned it up for having the interval determined by the plist

--- KillKey.py
+++ KillKey.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 import os
-from time import sleep
 from config import *
-while 1:
-    if os.system(the_command + " | grep " + device_uuid) == 0:
-        print("FOUND")
-    else:
-        print("NOT FOUND")
-        #os.system(logout) #this logs you out but doesn't reinit fileVault
-    sleep(sleep_duration)
+#while 1:
+if os.system(the_command + " | grep " + device_uuid) == 0:
+    print("FOUND")
+else:
+    print("NOT FOUND")
+    os.system(logout)
